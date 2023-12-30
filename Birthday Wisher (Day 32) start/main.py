9c95e78c33db0dff75856baa9416236bfd1f5673
krizhnaa <krizhnaprem@gmail.com>
2023-12-30 17:29:34 +0530

    Initializing SMTP Library

---
+++ Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,11 @@
+import smtplib
+
+my_mail = "krizhnatester@gmail.com"
+password = ""
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_mail, password=password)
+connection.sendmail(from_addr=my_mail, to_addrs="", msg="Heyy")
+connection.close()
+
