02a75e9deec24e5bfaae92e444c6d6fc4699dd5d
krizhnaa <krizhnaprem@gmail.com>
2023-12-30 17:34:57 +0530

    Made the code cleaner

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -3,9 +3,13 @@ import smtplib
 my_mail = "krizhnatester@gmail.com"
 password = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_mail, password=password)
-connection.sendmail(from_addr=my_mail, to_addrs="", msg="Heyy")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_mail, password=password)
+    connection.sendmail(
+        from_addr=my_mail,
+        to_addrs="",
+        msg="Heyy"
+    )
+
 
