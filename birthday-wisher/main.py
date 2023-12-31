69ff623adcbbc110fbeb1402839e0607d884b3c2
krizhnaa <krizhnaprem@gmail.com>
2023-12-31 16:21:19 +0530

    Implemented the datatime module for the proj

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,17 +1,27 @@
 import smtplib
 import datetime as dt
 import random
+import pandas as pd
+
+now = dt.datetime.now()
+curr_month = now.month
+curr_day = now.day
+
+df = pd.read_csv("birthdays.csv")
+
+if df[df['month'] == curr_month ]['month'].item() and df[df['day'] == curr_day ]['day'].item():
+    name = df[df['month'] == curr_month ]['name'].item()
+
 
 with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", "r+") as file:
     content = file.read()
-    content = content.replace('[NAME]', 'Krish')
+    content = content.replace('[NAME]', f"{name}")
     print(content)
 
 # my_mail = "krizhnatester@gmail.com"
 # password = ""
 #
-# now = dt.datetime.now()
-# day_of_week = now.weekday()
+
 #
 # if day_of_week == 6:
 #     with smtplib.SMTP("smtp.gmail.com") as connection:
