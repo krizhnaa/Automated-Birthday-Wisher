84d1666727ff984a3e65836db08eb4c5c180fa76
krizhnaa <krizhnaprem@gmail.com>
2023-12-31 15:39:12 +0530

    Sunday Motivation Mail

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,14 +1,23 @@
-
+import smtplib
 import datetime as dt
+import random
+
+with open("quotes.txt") as file:
+    all_quotes = file.readlines()
+    rand_quotes = all_quotes[random.randint(0, 101)]
+
+my_mail = "krizhnatester@gmail.com"
+password = ""
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
-hour = now.hour
-minute = now.minute
-if minute == 11:
-    print(minute)
+day_of_week = now.weekday()
 
-
-
+if day_of_week == 6:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_mail, password=password)
+        connection.sendmail(
+            from_addr=my_mail,
+            to_addrs="",
+            msg=f"Subject:Sunday Motivation\n\n{rand_quotes}"
+        )
