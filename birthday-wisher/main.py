80c09322adadcc714b75240c795f28b702d6da38
krizhnaa <krizhnaprem@gmail.com>
2023-12-31 16:05:12 +0530

    Reading the letters and replacing it with a name

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -2,25 +2,26 @@ import smtplib
 import datetime as dt
 import random
 
-with open("quotes.txt") as file:
-    all_quotes = file.readlines()
-    rand_quotes = all_quotes[random.randint(0, 101)]
+with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", "r+") as file:
+    content = file.read()
+    content = content.replace('[NAME]', 'Krish')
+    print(content)
 
-my_mail = "krizhnatester@gmail.com"
-password = ""
-
-now = dt.datetime.now()
-day_of_week = now.weekday()
-
-if day_of_week == 6:
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_mail, password=password)
-        connection.sendmail(
-            from_addr=my_mail,
-            to_addrs="",
-            msg=f"Subject:Sunday Motivation\n\n{rand_quotes}"
-        )
+# my_mail = "krizhnatester@gmail.com"
+# password = ""
+#
+# now = dt.datetime.now()
+# day_of_week = now.weekday()
+#
+# if day_of_week == 6:
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=my_mail, password=password)
+#         connection.sendmail(
+#             from_addr=my_mail,
+#             to_addrs="",
+#             msg=f"Subject:Sunday Motivation\n\n{rand_quotes}"
+#         )
 
 
 
